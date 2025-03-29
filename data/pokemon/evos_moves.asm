f8a9e09586ed55564cf353eeb686aa4678fe8cbb
KebabOnline <merek77777@gmail.com>
2025-03-29 21:17:17 +0100

    Gyarados moveset changes

--- data/pokemon/evos_moves.asm
+++ data/pokemon/evos_moves.asm
@@ -443,8 +443,10 @@ GyaradosEvosMoves:
 	db 0
 ; Learnset
 	db 20, BITE
-	db 25, DRAGON_RAGE
-	db 32, LEER
+	db 21, LEER
+	db 25, SLAM
+	db 30, WATERFALL
+	db 36, FIRE_BLAST
 	db 41, HYDRO_PUMP
 	db 52, HYPER_BEAM
 	db 0
